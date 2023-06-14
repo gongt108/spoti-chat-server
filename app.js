7e610e98f45910019a8df99cfa26ed3c8689f5d3
Rome Bell <romebell5@gmail.com>
2023-06-13 20:25:01 -0700

    feat: connect to Mongoose App database

---
+++ app.js
@@ -0,0 +1,4 @@
+const faker = require('@faker-js/faker');
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
---
+++ database-test.js
@@ -0,0 +1,23 @@
+// get access to environment variables
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+console.log('mongo uri =>', process.env.MONGO_URI);
+// connect to the database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// create connection object
+const db = mongoose.connection;
+
+// once the database opens
+db.once('open', () => {
+    console.log('Connected to MongoDB Database: Mongoose App at HOST: ', db.host, 'PORT: ', db.port);
+});
+
+// if there is a database error
+db.on('error', (err) => {
+    console.log(`Database error: `, err);
+});
