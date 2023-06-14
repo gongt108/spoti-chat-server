f09fdb891a52df589c277e76fe537ceaff46299b
Rome Bell <romebell5@gmail.com>
2023-06-13 22:52:22 -0700

    feat: add connection to database

---
+++ models/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// import all models
+const User = require('./user');
+
+console.log('mongo uri =>', process.env.MONGO_URI);
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// create connection object
+const db = mongoose.connection;
+
+// once the database opens
+db.once('open', () => {
+    console.log('Connected to MongoDB Database: Mongoose App at HOST: ', db.host, 'PORT: ', db.port);
+});
+
+// if there is a database error
+db.on('error', (err) => {
+    console.log(`Database error: `, err);
+});
+
+module.exports = {
+    User,
+}
