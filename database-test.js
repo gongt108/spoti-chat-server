a650a1fc96ae07995c5831dac09cabd8d037f2f9
Rome Bell <romebell5@gmail.com>
2023-06-13 22:19:53 -0700

    feat: add User model

--- database-test.js
+++ database-test.js
@@ -1,6 +1,11 @@
 // get access to environment variables
 require('dotenv').config();
 const mongoose = require('mongoose');
+const { faker } = require('@faker-js/faker');
+const { createRandomUser } = require('./utils');
+
+// import our models
+const User = require('./models/user');
 
 console.log('mongo uri =>', process.env.MONGO_URI);
 // connect to the database
@@ -21,3 +26,23 @@ db.once('open', () => {
 db.on('error', (err) => {
     console.log(`Database error: `, err);
 });
+
+// create a user and save to the DB
+User.create(createRandomUser())
+.then((user) => {
+    console.log('create user',user);
+})
+.catch((error) => {
+    console.log('error', error);
+});
+
+// create 100 users
+// for (let i = 0; i < 100; i++) {
+//     User.create(createRandomUser())
+//     .then((user) => {
+//         console.log(i, user);
+//     })
+//     .catch((error) => {
+//         console.log('error', error);
+//     });
+// }
