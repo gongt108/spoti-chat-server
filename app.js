9cde7f09a55dbe69fcf417e4fa4d8d592f69e80d
Rome Bell <romebell5@gmail.com>
2023-06-24 15:50:48 -0700

    test: add users GET and POST route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+    return res.json({ message: 'Welcome to my API' });
+});
+
 app.use('/posts', require('./controllers/posts'));
 app.use('/users', require('./controllers/users'));
 
@@ -16,3 +20,6 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server connected to PORT: ${PORT}`);
 });
+
+
+module.exports = app;
