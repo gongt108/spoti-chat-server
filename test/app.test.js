7125f571cfc9fce8baa833d0f5e0bd5610f01a33
Rome Bell <romebell5@gmail.com>
2023-06-24 18:06:44 -0700

    fix: uncomment test

--- test/app.test.js
+++ test/app.test.js
@@ -10,88 +10,88 @@ const { User } = require('../models');
 const { faker } = require('@faker-js/faker');
 const { createRandomUser } = require('../utils');
 
-// test home route
-// describe('GET /', () => {
-//     it('returns a 200 response', (done) => {
-//         request(app).get('/')
-//         .expect(200, done);
-//     });
-// });
+test home route
+describe('GET /', () => {
+    it('returns a 200 response', (done) => {
+        request(app).get('/')
+        .expect(200, done);
+    });
+});
 
 
-// // test users
-// describe('GET /users', () => {
-//     it('returns a 200 response', (done) => {
-//         request(app).get('/users')
-//         .expect(200, done);
-//     });
-//     it('returns a user with email', (done) => {
-//         request(app).get('/users')
-//         .then(result => {
-//             console.log('result', result._body.users[0]);
-//             expect(result._body.users[0]).to.have.property('email');
-//             done();
-//         })
-//     });
-//     it('should have more than 1 user', (done) => {
-//         request(app).get('/users')
-//         .then(result => {
-//             console.log('result', result._body.users);
-//             expect(result._body.users.length).to.be.above(1);// expect(10).to.be.above(5);
-//             done();
-//         })
-//     });
-// });
+// test users
+describe('GET /users', () => {
+    it('returns a 200 response', (done) => {
+        request(app).get('/users')
+        .expect(200, done);
+    });
+    it('returns a user with email', (done) => {
+        request(app).get('/users')
+        .then(result => {
+            console.log('result', result._body.users[0]);
+            expect(result._body.users[0]).to.have.property('email');
+            done();
+        })
+    });
+    it('should have more than 1 user', (done) => {
+        request(app).get('/users')
+        .then(result => {
+            console.log('result', result._body.users);
+            expect(result._body.users.length).to.be.above(1);// expect(10).to.be.above(5);
+            done();
+        })
+    });
+});
 
-// // test POST route
-// describe('POST /users/new', () => {
-//     it('should create a new user and have valid email', (done) => {
-//         const randomEmail = faker.internet.email();
-//         request(app).post('/users/new')
-//         .type('form')
-//         .send({
-//             streetAddress: "3560 Becker Burgs",
-//             city: "Miami",
-//             state: "Florida",
-//             zipCode: 98758,
-//             firstName: faker.person.firstName(),
-//             lastName: faker.person.lastName(),
-//             email: randomEmail,
-//             jobTitle: "Systems Engineer",
-//             birthdate: "1984-02-24T18:11:20.246Z",
-//             number: "(500) 519-6792",
-//         })
-//         .then(response => {
-//             console.log('new user created', response._body);
-//             expect(response._body.user.email).to.be.equal(randomEmail);
-//             done();
-//         })
-//         .catch(error => {
-//             console.log('error', error);
-//             throw error;
-//         })
-//     })
+// test POST route
+describe('POST /users/new', () => {
+    it('should create a new user and have valid email', (done) => {
+        const randomEmail = faker.internet.email();
+        request(app).post('/users/new')
+        .type('form')
+        .send({
+            streetAddress: "3560 Becker Burgs",
+            city: "Miami",
+            state: "Florida",
+            zipCode: 98758,
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: randomEmail,
+            jobTitle: "Systems Engineer",
+            birthdate: "1984-02-24T18:11:20.246Z",
+            number: "(500) 519-6792",
+        })
+        .then(response => {
+            console.log('new user created', response._body);
+            expect(response._body.user.email).to.be.equal(randomEmail);
+            done();
+        })
+        .catch(error => {
+            console.log('error', error);
+            throw error;
+        })
+    })
 
 
-//     it('returns a 200 response', (done) => {
-//         const randomEmail = faker.internet.email();
-//         request(app).post('/users/new')
-//         .type('form')
-//         .send({
-//             streetAddress: "3560 Becker Burgs",
-//             city: "Miami",
-//             state: "Florida",
-//             zipCode: 98758,
-//             firstName: faker.person.firstName(),
-//             lastName: faker.person.lastName(),
-//             email: randomEmail,
-//             jobTitle: "Systems Engineer",
-//             birthdate: "1984-02-24T18:11:20.246Z",
-//             number: "(500) 519-6792",
-//         })
-//         .expect(200, done);
-//     })
-// });
+    it('returns a 200 response', (done) => {
+        const randomEmail = faker.internet.email();
+        request(app).post('/users/new')
+        .type('form')
+        .send({
+            streetAddress: "3560 Becker Burgs",
+            city: "Miami",
+            state: "Florida",
+            zipCode: 98758,
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: randomEmail,
+            jobTitle: "Systems Engineer",
+            birthdate: "1984-02-24T18:11:20.246Z",
+            number: "(500) 519-6792",
+        })
+        .expect(200, done);
+    })
+});
 
 // PUT /users/:id
 describe('PUT /users/:id', () => {
