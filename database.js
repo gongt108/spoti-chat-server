764dc17f4878a88c26c56c1fa147350daa5f60f5
Rome Bell <romebell5@gmail.com>
2023-12-12 22:49:46 -0800

    chore: add create for Game

--- database.js
+++ database.js
@@ -1,7 +1,10 @@
 // get access to environment variables
 require('dotenv').config();
 const mongoose = require('mongoose');
-// import our models
+// import our database
+const { 
+    Game 
+} = require('./models'); // automatically looks for the index.js file by default
 // console.log('uri', process.env.MONGO_URI);
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -18,3 +21,18 @@ db.once('open', () => console.log('Connected to facebook database', 'host:', db.
 
 // if database has an error
 db.on('error', (error) => console.log('database error', error));
+
+// create a game and print the new game
+
+Game.create({ 
+    title: 'Mortal Kombat', 
+    publisher: 'Midway', 
+    releaseYear: 1992, 
+    price: 59, 
+    genre: 'fighting',
+    rating: 5
+})
+.then(result => {
+    console.log(result);
+})
+.catch(error => console.log(error));
