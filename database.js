ffb72392c0cee75d84017052a434e3abd6a44d4b
Rome Bell <romebell5@gmail.com>
2023-12-12 22:51:43 -0800

    fix: remove db connection on database.js file

--- database.js
+++ database.js
@@ -5,32 +5,15 @@ const mongoose = require('mongoose');
 const { 
     Game 
 } = require('./models'); // automatically looks for the index.js file by default
-// console.log('uri', process.env.MONGO_URI);
-const MONGO_URI = process.env.MONGO_URI;
-
-// connect to database
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-// create a connection object
-const db = mongoose.connection;
-
-// once database connection opens
-db.once('open', () => console.log('Connected to facebook database', 'host:', db.host, 'port:', db.port));
-
-// if database has an error
-db.on('error', (error) => console.log('database error', error));
 
 // create a game and print the new game
-
 Game.create({ 
-    title: 'Mortal Kombat', 
-    publisher: 'Midway', 
-    releaseYear: 1992, 
-    price: 59, 
-    genre: 'fighting',
-    rating: 5
+    title: 'NBA 2k', 
+    publisher: '---', 
+    releaseYear: 2023, 
+    price: 69, 
+    genre: 'sports',
+    rating: 4.5
 })
 .then(result => {
     console.log(result);
