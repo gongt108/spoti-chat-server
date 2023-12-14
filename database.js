c79f06e2983178a33145e41409e60b77c3b81cbb
Rome Bell <romebell5@gmail.com>
2023-12-13 18:59:13 -0800

    chore: add Game result in comment form

--- database.js
+++ database.js
@@ -8,14 +8,29 @@ const {
 
 // create a game and print the new game
 Game.create({ 
-    title: 'NBA 2k', 
-    publisher: '---', 
-    releaseYear: 2023, 
+    title: 'FIFA', 
+    publisher: 'EA Sports', 
+    releaseYear: 2024, 
     price: 69, 
     genre: 'sports',
-    rating: 4.5
+    rating: 4.7
 })
 .then(result => {
     console.log(result);
+    /**
+     * result
+    {
+        title: 'FIFA',
+        rating: 4.7,
+        releaseYear: 2024,
+        genre: 'sports',
+        price: 69,
+        publisher: 'EA Sports',
+        _id: new ObjectId("657a6eede59253c578b4e3c4"),
+        createdAt: 2023-12-14T02:56:45.456Z,
+        updatedAt: 2023-12-14T02:56:45.456Z,
+        __v: 0
+    }
+    */
 })
 .catch(error => console.log(error));
