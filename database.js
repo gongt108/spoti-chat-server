57fc673a8484f30f54a68c00b2cc0bada8839e1b
Rome Bell <romebell5@gmail.com>
2023-12-13 19:26:16 -0800

    feat: add READ for games

--- database.js
+++ database.js
@@ -34,3 +34,58 @@ Game.create({
     */
 })
 .catch(error => console.log(error));
+
+
+// READ A GAME
+// ------------------------------------
+// READ: find all games 
+// ------------------------------------
+Game.find({})
+.then(games => {
+    console.log('--- games ---\n', games);
+})
+.catch(error => {
+    console.log('--- read all games error ---\n', error);
+});
+
+// ------------------------------------
+// READ: find all games released in 2024
+// ------------------------------------
+Game.find({ releaseYear: 2024 })
+.then(games => {
+    console.log('--- games releaseYear 2024 ---\n', games);
+})
+.catch(error => {
+    console.log('--- read error releaseYear ---\n', error);
+});
+
+// ------------------------------------------
+// READ: find one game - NBA 2K Kobe Edition
+// ------------------------------------------
+Game.findOne({ title: 'NBA 2K Kobe Edition' })
+.then(game => {
+    console.log('--- games title NBA 2K Kobe Edition ---\n', game);
+})
+.catch(error => {
+    console.log('--- read NBA 2K... error ---\n', error);
+});
+
+// ----------------------------------------------
+// READ: find one game - find it by the ObjectId
+// ----------------------------------------------
+Game.findOne({ _id: "657506e161b1d1b53aa0ff18" })
+.then(game => {
+    console.log('--- games search by ObjectId ---\n', game);
+    /**
+     * game
+     --- games search by ObjectId ---
+    {
+        _id: new ObjectId("657506e161b1d1b53aa0ff18"),
+        title: 'NBA 2K Kobe Edition',
+        releaseYear: 2024
+    }
+    */
+})
+.catch(error => {
+    console.log('--- read NBA 2K... error ---\n', error);
+});
