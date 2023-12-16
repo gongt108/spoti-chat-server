d797803582cf0c32e36ce24f7c53b82b64049a96
Rome Bell <romebell5@gmail.com>
2023-12-16 11:37:36 -0800

    Create video.test.js

---
+++ test/video.test.js
@@ -0,0 +1,18 @@
+// import the server
+const app = require('../app'); // make the server is exported from app.js
+// what's needed for testing
+const request = require('supertest');
+const expect = require('chai').expect;
+// import the modules
+const { Video } = require('../models');
+
+// import faker
+const { faker } = require('@faker-js/faker');
+
+test home route
+describe('GET /', () => {
+    it('returns a 200 response', (done) => {
+        request(app).get('/')
+        .expect(200, done);
+    });
+});
