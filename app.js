c6219b01d6dd2a9a57a8dbbbf8cd57cf6b5a7ad7
Tiffany Gong <gongt108@gmail.com>
2023-12-30 17:41:23 -0800

    edit app.js

--- app.js
+++ app.js
@@ -20,33 +20,41 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-	secret: 'your-secret-key',  // Replace with a secure secret
-	resave: false,
-	saveUninitialized: false,
-}));
+app.use(
+	session({
+		secret: 'your-secret-key', // Replace with a secure secret
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Passport strategy (you can put this in a separate file if needed)
-passport.use(new LocalStrategy({
-	usernameField: 'email',
-	passwordField: 'password',
-}, (email, password, cb) => {
-	db.user.findOne({ email })
-    .then(user => {
-      if (!user || !user.validPassword(password)) {
-        return cb(null, false);
-      }
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password',
+		},
+		(email, password, cb) => {
+			db.user
+				.findOne({ email })
+				.then((user) => {
+					if (!user || !user.validPassword(password)) {
+						return cb(null, false);
+					}
 
-      // Your additional authentication logic goes here
-      // For example, check user roles, update last login timestamp, etc.
+					// Your additional authentication logic goes here
+					// For example, check user roles, update last login timestamp, etc.
 
-      return cb(null, user);
-    })
-    .catch(err => cb(err));
-}));
+					return cb(null, user);
+				})
+				.catch((err) => cb(err));
+		}
+	)
+);
 
 // Passport serialization and deserialization (you can put this in a separate file if needed)
 passport.serializeUser((user, cb) => {
@@ -58,7 +66,6 @@ passport.deserializeUser((id, cb) => {
 	// This may vary based on your database setup
 });
 
-
 app.get('/', (req, res) => {
 	return res.json({ message: 'Welcome to the Spoti-Chat App' });
 });
@@ -66,7 +73,6 @@ app.get('/', (req, res) => {
 // import controllers
 app.use('/posts', require('./controllers/posts'));
 app.use('/users', require('./controllers/users'));
-app.use()
 
 app.post('/refresh', (req, res) => {
 	const refreshToken = req.body.refreshToken;
