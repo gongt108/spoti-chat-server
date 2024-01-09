e265553d7efa803f4a80e089d28b9482f94dd259
Tiffany Gong <gongt108@gmail.com>
2024-01-09 07:59:09 -0800

    temporary test

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	return res.json({ message: REDIRECT_URI });
+	return res.json({ message: `Test 2 ${REDIRECT_URI}` });
 });
 
 // import controllers
