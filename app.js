3e72b7e1ce2bcb2374c704dd26d17ef29f69d459
Tiffany Gong <gongt108@gmail.com>
2024-01-09 03:36:49 -0800

    temporary test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	return res.json({ message: 'Welcome to the Spoti-Chat App' });
+	return res.json({ message: process.env.REDIRECT_URI });
 });
 
 // import controllers
